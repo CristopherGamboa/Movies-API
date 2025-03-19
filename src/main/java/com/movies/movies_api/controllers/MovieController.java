fb2af51e7c983ce336e053248cd60077a336ea40
CristopherGamboa <cristophergamboa@outlook.com>
2025-03-18 22:25:55 -0300

    Add Movie Rest Controller

---
+++ src/main/java/com/movies/movies_api/controllers/MovieController.java
@@ -0,0 +1,50 @@
+package com.movies.movies_api.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.movies.movies_api.interfaces.MovieService;
+import com.movies.movies_api.models.Movie;
+
+@RestController
+@RequestMapping("api/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable Long id) {
+        return movieService.findById(id);
+    }
+
+    @PostMapping
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieService.save(movie);
+    }
+
+    @PutMapping("/{id}")
+    public Movie updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        return movieService.update(id, movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable Long id) {
+        movieService.delete(id);
+    }
+}
