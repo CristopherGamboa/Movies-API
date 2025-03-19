16e440e2612b11b5e1a394dfe5e8b17087b72b60
CristopherGamboa <cristophergamboa@outlook.com>
2025-03-18 22:24:24 -0300

    Add Movie Service interface and implementation

---
+++ src/main/java/com/movies/movies_api/services/MovieServiceImp.java
@@ -0,0 +1,43 @@
+package com.movies.movies_api.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.movies.movies_api.interfaces.MovieService;
+import com.movies.movies_api.models.Movie;
+import com.movies.movies_api.repositories.InMemoryMovieRepository;
+
+@Service
+public class MovieServiceImp implements MovieService {
+    private final InMemoryMovieRepository movieRepository;
+
+    public MovieServiceImp(InMemoryMovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    @Override
+    public Movie findById(Long id) {
+        return movieRepository.findById(id);
+    }
+
+    @Override
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+
+    @Override
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public Movie update(Long id, Movie movie) {
+        return movieRepository.update(id, movie);
+    }
+
+    @Override
+    public void delete(Long id) {
+        movieRepository.delete(id);
+    }
+}
