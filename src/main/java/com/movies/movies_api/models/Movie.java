9e828519897985bcf914df2c19d0c0fd98a6fdff
CristopherGamboa <cristophergamboa@outlook.com>
2025-04-04 23:48:27 -0300

    Implement Lombok and set movie as persistance entity

--- src/main/java/com/movies/movies_api/models/Movie.java
+++ src/main/java/com/movies/movies_api/models/Movie.java
@@ -1,56 +1,29 @@
 package com.movies.movies_api.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "movie")
 public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private int year;
-    private String[] genres;
+    private String genre;
     private String director;
     private String synopsis;
-
-    public Movie(Long id, String title, int year, String[] genres, String director, String synopsis) {
-        this.id = id;
-        this.title = title;
-        this.year = year;
-        this.genres = genres;
-        this.director = director;
-        this.synopsis = synopsis;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public int getYear() {
-        return year;
-    }
-    public void setYear(int year) {
-        this.year = year;
-    }
-    public String[] getGenres() {
-        return genres;
-    }
-    public void setGenres(String[] genres) {
-        this.genres = genres;
-    }
-    public String getDirector() {
-        return director;
-    }
-    public void setDirector(String director) {
-        this.director = director;
-    }
-    public String getSynopsis() {
-        return synopsis;
-    }
-    public void setSynopsis(String synopsis) {
-        this.synopsis = synopsis;
-    }
 }
